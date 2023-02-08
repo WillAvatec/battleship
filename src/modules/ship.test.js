ec458fabde2f09e8a8758e3573d5f1fcd3326e71
Will <avatec2312@gmail.com>
2023-02-08 18:05:50 -0500

    test used to comprobe logic behind ship factory

---
+++ src/modules/ship.test.js
@@ -0,0 +1,29 @@
+import Ship from './ship.js'
+
+describe('BattleShip object',()=>{
+    let boat;
+    beforeEach(()=>{
+        boat = new Ship(2)
+    })
+    
+    test('hits should increment only when hits is lower than length',()=>{
+        boat.hit();
+        expect(boat.hit()).toBe(2)
+    })
+
+    it('should return undefined if hits is greater than or equal to length', ()=>{
+        boat.hit();
+        boat.hit();
+        expect(boat.hit()).toBeUndefined()
+    })
+
+    test('isSunk method to return false by default', ()=>{
+        expect(boat.isSunk()).toBe(false)
+    })
+
+    it('should return true if hits is >= length',()=>{
+        boat.hit();
+        boat.hit();
+        expect(boat.isSunk()).toBe(true);
+    })
+})
