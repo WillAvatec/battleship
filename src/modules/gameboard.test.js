4266c8094989b8d0a0672a110950df0c2707f2b9
Will <avatec2312@gmail.com>
2023-02-10 17:12:34 -0500

    New tests for setShip function

--- src/modules/gameboard.test.js
+++ src/modules/gameboard.test.js
@@ -5,7 +5,7 @@ import Ship from './ship'
 describe('testing Gameboard Instance array',()=>{
     let mapa;
     beforeEach(()=>{
-        mapa = new GameBoard()
+        mapa = new GameBoard();
     })
 
     it('should have a 2D array working as the board',()=>{
@@ -21,11 +21,12 @@ describe('testing Gameboard Instance array',()=>{
 
 describe('testing setShip method',()=>{
     let mapa;
-    let boat = new Ship();
+    let boat;
     let coord = [0,0];
     let prevState;
     beforeEach(()=>{
         mapa = new GameBoard();
+        boat = new Ship();
         prevState = [...mapa.getBoard()]
     })
 
@@ -40,6 +41,24 @@ describe('testing setShip method',()=>{
     it('should set ship on coordinates with boat{size:1}',()=>{
         mapa.setShip(boat, coord);
         expect(mapa.getBoard(coord)).toBe(1)
+    })
+
+    it('should set ship{size:3} on horizontal',()=>{
+        boat = new Ship(3);
+        let coord = [0,0]
+        mapa.setShip(boat,coord);
+        for(let i=0; i < boat.getSize() ;i++){
+            expect(mapa.getBoard([coord[0]+i,coord[1]])).toBe(1)
+        }
+    })
+
+    it('should set ship{size:3 in vertical',()=>{
+        boat = new Ship(3);
+        let coord = [0,0]
+        mapa.setShip(boat,coord);
+        for(let i=0; i < boat.getSize() ;i++){
+            expect(mapa.getBoard([coord[0],coord[1]]+i)).toBe(1)
+        }
     })
 
     it('should only set ship if space available',()=>{
