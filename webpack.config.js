65e6fe5c3c236c29de3de8ed2f6263e251165ac3
Will <avatec2312@gmail.com>
2023-02-13 18:44:05 -0500

    Using css-loader to bundle css file

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,19 @@ const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
-  plugins: [new ESLintPlugin(options)]
+  mode: 'development',
+  plugins: [new ESLintPlugin()],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      // Additional configuration to handle *.css files
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
